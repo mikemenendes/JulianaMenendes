310aeb9c9346961b07b44e46abf1ca8311521fa1
Mike  Menendes <103233984+mikemenendes@users.noreply.github.com>
2023-10-08 10:09:20 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Josefin+Sans&display=swap" rel="stylesheet">
+    <title>JulianaMenendes</title>
+</head>
+<body>
+    
+<?php include('menu.php'); ?>
+
+<section class="banner"></section>
+
+<section class="titulo spaceud">
+    <div class="container8 center">
+
+        <div class="titulo_ spaceud">
+            <h2>Portfolio</h2>
+        </div>
+
+        <div class="line center">
+            <div class="line_">
+                <i class="fa-solid fa-heart fa-flip"></i>
+            </div>
+        </div>
+    
+    </div>
+</section>
+
+<section class="galeria spaceud">
+    <div class="container center">
+        <img src="img/img1.jpg" alt="" width="280px" height="300px">
+        <img src="img/img2.jpg" alt="" width="280px" height="300px">
+        <img src="img/img3.jpg" alt="" width="280px" height="300px">
+        <img src="img/img4.jpg" alt="" width="280px" height="300px">
+        <img src="img/img5.jpg" alt="" width="280px" height="300px">
+        <img src="img/img6.jpg" alt="" width="280px" height="300px">
+        <img src="img/img7.jpg" alt="" width="280px" height="300px">
+        <img src="img/img8.jpg" alt="" width="280px" height="300px">
+        <img src="img/img9.jpg" alt="" width="280px" height="300px">
+        <img src="img/bannerdaju.jpg" alt="" width="280px" height="300px">
+        <img src="img/img1.jpg" alt="" width="280px" height="300px">
+        <img src="img/img2.jpg" alt="" width="280px" height="300px">
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Josefin+Sans&display=swap" rel="stylesheet">
+    <title>JulianaMenendes</title>
+</head>
+<body>
+    <?php ?>
+        
+        <header> 
+            <div class="container space">
+
+                <div class="logo">
+                    <a href="#">Juliana Menendes</a>
+                </div>
+
+                <div class="social right">
+                    <i class="fa-brands fa-facebook-f "></i><a href="#"> Facebook</a>
+                    <i class="fa-brands fa-instagram "></i><a href="#"> instagram</a>
+                    <i class="fa-brands fa-pinterest-p "></i><a href="#"> pinterest</a>
+                    <i class="fa-brands fa-youtube "></i><a href="#"> youtube</a>
+                </div>
+                    <div class="clear"></div>
+            </div>
+        </header>
+
+    <?php ?>
+</body>
+</html>
